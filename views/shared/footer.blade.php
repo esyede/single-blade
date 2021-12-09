456aee5e0d621ccaf34ef45bd3c7abad4beac101
esyede <suyadi.1992@gmail.com>
2021-12-10 03:56:47 +0700

    :bug: fix wrong class import

--- views/shared/footer.blade.php
+++ views/shared/footer.blade.php
@@ -1,4 +1,4 @@
 <div id="footer">
 	<span class="copyright">Written by <a href="{{ $link }}" target="_blank">@esyede</a></span>
-	<span class="version">Version {{ Blade::VERSION }}</span>
+	<span class="version">Version {{ Esyede\Blade::VERSION }}</span>
 </div>
